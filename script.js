3e04ba0f0773519ae5fc257027a634c39ad25dc9
Migara Bhanu <76873393+migarabhanu@users.noreply.github.com>
2023-01-01 11:08:47 +0530

    script.js created

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+  $(document).ready(function () {
+    //Preloader
+    preloaderFadeOutTime = 100;
+  function hidePreloader() {
+        var preloader = $('.spinner-wrapper');
+  preloader.fadeOut(preloaderFadeOutTime);
+      }
+  hidePreloader();
+    });
+</script>
